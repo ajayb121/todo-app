0c6fe86ae466b0850e5609fc3c4cb7922fa37c07
ajayb121 <ajaybrahma93@gmail.com>
2023-10-29 17:30:03 +0000

    update styles

--- src/app/components/Todo.tsx
+++ src/app/components/Todo.tsx
@@ -5,12 +5,33 @@ import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 import { TodoInterface } from '@/types/todo';
 
-export default function Todo() {
-  const [todos, setTodos] = useState<TodoInterface[]>([]);
+// Initial mock data for todos
+const mockTodoData: TodoInterface[] = [{
+  id: 4,
+  title: 'Todo item 1',
+  isComplete: false,
+}, {
+  id: 3,
+  title: 'Completed item 1',
+  isComplete: true,
+}, {
+  id: 2,
+  title: 'Completed item 2',
+  isComplete: true,
+}, {
+  id: 1,
+  title: 'Todo item 2',
+  isComplete: false,
+}];
 
+export default function Todo() {
+  // State to manage the list of todos
+  const [todos, setTodos] = useState<TodoInterface[]>(mockTodoData);
+
+  // Function to add a new todo
   const addTodo = (title: string): void => {
     // setting an id for each todo item
-    const todoId = todos.length ? todos[todos.length - 1].id + 1 : 1;
+    const todoId = todos.length ? todos[0].id + 1 : 1;
     const todoItem = {
       id: todoId,
       title,
@@ -24,36 +45,39 @@ export default function Todo() {
     ]);
   };
 
+  // Function to remove a todo
   const removeTodo = (itemId: number): void => {
     const updatedTodos = [...todos];
 
-    // find index of the id and remove it from the list
+    // Find the index of the todo with the given id and remove it from the list
     const index = updatedTodos.findIndex(({ id }) => id === itemId);
     updatedTodos.splice(index, 1);
 
     setTodos(updatedTodos);
   };
 
+  // Function to toggle the completion status of a todo
   const toggleComplete = (itemId: number): void => {
     const updatedTodos = [...todos];
 
-    // find index of the id and toggle isComplete value
+    // Find the index of the todo with the given id and toggle its 'isComplete' value
     const index = updatedTodos.findIndex(({ id }) => id === itemId);
     updatedTodos[index].isComplete = !updatedTodos[index].isComplete;
 
     setTodos(updatedTodos);
   };
 
+  // Function to remove completed todos
   const removeCompleted = (): void => {
-    // filter completed todos from the list and update todos
+    // Filter out completed todos from the list and update the todos state
     const updatedTodos = todos.filter(({ isComplete }) => !isComplete);
     setTodos(updatedTodos);
   }
 
   return (
-    <div>
+    <>
       <TodoInput onAdd={addTodo} />
       <TodoList todos={todos} removeTodo={removeTodo} toggleComplete={toggleComplete} removeCompleted={removeCompleted} />
-    </div>
+    </>
   );
 }
--- src/app/components/TodoInput.tsx
+++ src/app/components/TodoInput.tsx
@@ -7,27 +7,35 @@ interface TodoInputProps {
 const TodoInput = ({ onAdd }: TodoInputProps): JSX.Element => {
   const [inputText, setInputText] = useState("");
 
+  // Handler for input text change
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputText(event.target.value);
   };
 
+  // Handler for adding a new todo
   const addToDo = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (inputText) {
-      onAdd(inputText);
-      setInputText("");
+      onAdd(inputText); // Call the onAdd function to add the new todo
+      setInputText(""); // Clear the input field
     }
   };
 
   return (
-    <form onSubmit={addToDo}>
+    <form onSubmit={addToDo} className="w-full flex">
       <input
         type="text"
-        className="border border-gray-700 rounded p-4 w-full text-lg"
+        className="border border-gray-700 rounded p-3 sm:p-4 text-md sm:text-lg grow"
         placeholder="Add a new todo"
         value={inputText}
         onChange={handleInputChange}
       />
+      <button
+        type="submit"
+        className="p-3 sm:py-4 sm:px-6 rounded text-md sm:text-lg ml-3 bg-violet-600 text-slate-50 font-semibold hover:bg-violet-700"
+      >
+        Submit
+      </button>
     </form>
   );
 }
